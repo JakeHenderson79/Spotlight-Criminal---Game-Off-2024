3585e230b2f5edb3812b65f207685d334cbc5ed6
JakeHenderson21 <23626171@stu.mmu.ac.uk>
2024-11-12 22:04:51 +0000

    Added prototype system to prevent robber getting stuck
    Added a prototype system that will prevent the robber from getting stuck on the wall, the robber is still not as smooth as it can be when transitioning between blocks but that will change soon.

--- Assets/Scripts/Robber.cs
+++ Assets/Scripts/Robber.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Robber : MonoBehaviour
 {
@@ -13,6 +14,13 @@ public class Robber : MonoBehaviour
     [SerializeField] private List<Transform> currentConnectors = new List<Transform>();
     [SerializeField] private Transform connectedConnector;
     private bool foundRoute;
+    private Vector3 startCast;
+    private Vector3 upCast ,upCast2;
+    private Vector3 leftCast, leftCast2;
+    private Vector3 rightCast, rightCast2;
+    private Vector3 downCast, downCast2;
+    public LayerMask myLayerMask;
+   [SerializeField] private bool upWall, downWall, leftWall, rightWall;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,15 +30,152 @@ public class Robber : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        startCast = gameObject.transform.position;
+        upCast = new Vector3(transform.position.x - 0.5f, transform.position.y + 0.5f, transform.position.z);
+        upCast2 = new Vector3(transform.position.x + 0.5f, transform.position.y + 0.5f, transform.position.z);
+
+        downCast = new Vector3(transform.position.x - 0.5f, transform.position.y - 0.5f, transform.position.z);
+        downCast2 = new Vector3(transform.position.x + 0.5f, transform.position.y - 0.5f, transform.position.z);
+
+
+        leftCast = upCast;
+        leftCast2 = downCast;
+
+        rightCast = upCast2;
+        rightCast2 = downCast2;
+
+      
+        Debug.DrawLine(upCast, new Vector3(transform.position.x - 0.5f, transform.position.y + 1, transform.position.z), Color.yellow);
+        Debug.DrawLine(upCast2, new Vector3(transform.position.x + 0.5f, transform.position.y + 1, transform.position.z), Color.yellow);
+        RaycastHit2D upMovement = Physics2D.Raycast(upCast, transform.up,1f,myLayerMask);
+        RaycastHit2D upMovement2 = Physics2D.Raycast(upCast2,transform.up,1f,myLayerMask);
+        if(upMovement.collider != null)
+        {
+            Debug.Log(upMovement.collider);
+            if(upMovement.collider.tag == "Wall")
+            {
+                upWall = true;
+            }
+            
+        }else if (upMovement2.collider != null)
+        {
+            Debug.Log(upMovement.collider);
+            if (upMovement2.collider.tag == "Wall")
+            {
+                upWall = true;
+            }
+        }
+        else
+        {
+            upWall = false;
+        }
+
+        Debug.DrawLine(downCast, new Vector3(transform.position.x - 0.5f, transform.position.y - 1, transform.position.z), Color.green);
+        Debug.DrawLine(downCast2, new Vector3(transform.position.x + 0.5f, transform.position.y - 1, transform.position.z), Color.green);
+        RaycastHit2D downMovement = Physics2D.Raycast(downCast,-transform.up,1f,myLayerMask);
+        RaycastHit2D downMovement2 = Physics2D.Raycast(downCast2, -transform.up,1f,myLayerMask);
+        if (downMovement.collider != null)
+        {
+            if (downMovement.collider.tag == "Wall")
+            {
+                downWall = true;
+            }
+            
+        }else if(downMovement2.collider != null)
+        {
+            if (downMovement2.collider.tag == "Wall")
+            {
+                downWall = true;
+            }
+        }
+        else
+        {
+            downWall = false;
+        }
+
+        Debug.DrawLine(leftCast, new Vector3(transform.position.x - 1, transform.position.y + 0.5f, transform.position.z), Color.cyan);
+        Debug.DrawLine(leftCast2,new Vector3(transform.position.x - 1, transform.position.y - 0.5f, transform.position.z), Color.cyan);
+        RaycastHit2D leftMovement = Physics2D.Raycast(leftCast, -transform.right, 1f, myLayerMask);
+        RaycastHit2D leftMovement2 = Physics2D.Raycast(leftCast2, -transform.right, 1f, myLayerMask);
+        if (leftMovement.collider != null)
+        {
+            if (leftMovement.collider.tag == "Wall")
+            {
+                leftWall = true;
+            }
+        }else if (leftMovement2.collider != null)
+        {
+            if (leftMovement2.collider.tag == "Wall")
+            {
+                leftWall = true;
+            }
+        }
+        else
+        {
+            leftWall = false;
+        }
+
+        Debug.DrawLine(rightCast, new Vector3(transform.position.x + 1, transform.position.y + 0.5f, transform.position.z), Color.magenta);
+        Debug.DrawLine(rightCast2, new Vector3(transform.position.x + 1, transform.position.y - 0.5f, transform.position.z), Color.magenta);
+        RaycastHit2D rightMovement = Physics2D.Raycast(rightCast, transform.right, 1f, myLayerMask);
+        RaycastHit2D rightMovement2 = Physics2D.Raycast(rightCast2, transform.right, 1f, myLayerMask);
+        if (rightMovement.collider != null)
+        {
+            Debug.Log(rightMovement.collider + ", " + rightMovement2.collider);
+            if (rightMovement.collider.tag == "Wall")
+            {
+                rightWall = true;
+            }
+
+        }else if (rightMovement2.collider != null)
+        {
+            Debug.Log(rightMovement.collider + ", " + rightMovement2.collider);
+            if (rightMovement2.collider.tag == "Wall")
+            {
+                rightWall = true;
+            }
+        }
+        else
+        {
+            rightWall = false;
+        }
     }
     private void FixedUpdate()
     {
         if(connector  != null)
         {
-            Vector3 direction = connector.position - transform.position;
+          
+            Vector3 direction = connector.position - transform.position;         
             direction.Normalize();
+
+            if (upWall && downWall)
+            {
+                direction.y = 0;
+            }
+            else if (upWall && !downWall)
+            {
+                direction.y -= 1;
+            }
+            else if (!upWall && downWall)
+            {
+                direction.y += 1;
+            }
+
+            if (leftWall && rightWall)
+            {
+                direction.x = 0;
+            }
+            else if (leftWall && !rightWall)
+            {
+                direction.x += 1;
+            }
+            else if (!leftWall && rightWall)
+            {
+                direction.x -= 1;
+            }
+            Debug.Log("Up " + upWall + ", Down " + downWall + ", Left " + leftWall + ", Right " + rightWall);
             movement = direction;
+           // Debug.Log(movement);
             moveRobber(movement);
         }
     

06289a1f48fdd7f98f8ed09d49a2ddf81d9d7897
JakeHenderson21 <23626171@stu.mmu.ac.uk>
2024-11-16 18:28:55 +0000

    Added detection
    Added a detection object to appear when the robber collides with a spotlight

--- Assets/Scripts/Spotlight.cs
+++ Assets/Scripts/Spotlight.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Spotlight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform robberNear;
+    private Transform warning;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.tag == "Robber")
+        {
+            Vector3 direction = collision.transform.position - transform.position;
+            direction.Normalize();
+            warning = Instantiate(robberNear,transform.position  + direction * 1.25f,transform.rotation);
+            warning.position = new Vector3(warning.position.x,warning.position.y,-2f);
+            Invoke("destroySymbol", 2f);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void destroySymbol()
     {
-        
+        Destroy(warning.gameObject);
     }
 }

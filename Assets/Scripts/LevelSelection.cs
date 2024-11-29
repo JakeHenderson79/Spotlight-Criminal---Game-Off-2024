c2a9c96effc9e1df732c2f9e461ae55fdc4117e2
JakeHenderson21 <23626171@stu.mmu.ac.uk>
2024-11-29 17:36:14 +0000

    Added further functionality to menu

--- Assets/Scripts/LevelSelection.cs
+++ Assets/Scripts/LevelSelection.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,9 +8,14 @@ using UnityEngine.SceneManagement;
 public class LevelSelection : MonoBehaviour
 {
     [SerializeField] private string levelName;
+    [SerializeField] private string levelID;
+    private void Start()
+    {
+        gameObject.transform.Find("LevelName").GetComponent<TextMeshProUGUI>().text = levelName;
+    }
     public void levelOpener()
     {
-        SceneManager.LoadScene(levelName);
+        SceneManager.LoadScene(levelID);
     }
    
 }
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] private GameObject mainMenu;
+
     public void playButton()
     {
-
+        mainMenu.SetActive(false);
+        GameObject.Find("LevelSelection").SetActive(true);
     }
     public void tutorialButton()
     {
-
+        mainMenu.SetActive(false);
+        GameObject.Find("Tutorial").SetActive(true);
+    }
+    public void settingsButton()
+    {
+        mainMenu.SetActive(false);
+        GameObject.Find("Settings").SetActive(true);
     }
     public void exitButton()
     {
         Application.Quit();
+    }
+    public void backButton()
+    {
+        GameObject.Find("LevelSelection").SetActive(false);
+        mainMenu.SetActive(true);
+
     }
 }
